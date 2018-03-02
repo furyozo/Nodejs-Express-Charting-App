e42b0ee1fabc3f6cd01e47c8e22943fda67b3d72
furyozo <bodyboro@seznam.cz>
2018-03-02 13:27:17 +0100

    stock data is now formatted for front end outputting through web sockets

--- controllers/websockets.js
+++ controllers/websockets.js
@@ -9,28 +9,20 @@ module.exports = class WS {
    * runs the web socket server at  specified port
    */
   static run(port) {
+    var WS = this;
     var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: port})
     wss.on('connection', function (ws) {
       ws.on('message', function (message) {
         console.log('received: %s', message)
 
-        yahooFinance.historical({
-          symbol: message,
-          from: '2015-01-01',
-          to: new Date().toISOString().slice(0,10),
-          period: 'm'
-          // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-        }, function (err, quotes) {
+        Stock.add(message, function(err, stock) {
           if (err) console.error(err);
-          else if (quotes.length === 0) console.error("stock does not exist");
-          else {
-            Stock.add(message, function(err, stock) {
-              if (err) console.error(err);
-            })
-            console.log(quotes);
-          }
-        });
+          Stock.getChartableStocks(function(arr) {
+            console.log(arr);
+            ws.send(['hey', 'azsd']);
+          });
+        })
 
       })
       // setInterval(
--- models/Stock.js
+++ models/Stock.js
@@ -1,3 +1,5 @@
+var yahooFinance = require('yahoo-finance');
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/')
 const db = mongoose.connection
@@ -42,15 +44,62 @@ StockSchema.statics.add = function (name, callback) {
    // use schema.create to insert data into the db
    else {
      Stock.create(stockData, function (err, stock) {
-       if (err) {
-         callback(err)
-       } else {
-         callback(null, stock);
-       }
+       if (err) callback(err)
+       else callback(null, stock);
      });
    }
  });
 
+}
+
+/**
+ * get single stock in chartable format
+ * @param  {Function} callback callback function
+ */
+ StockSchema.statics.getChartableStock = function (name, callback) {
+   // create new empty object
+   var stockObj = {
+     name: '',
+     values: ''
+   };
+   // get the stock name
+   yahooFinance.quote({
+     symbol: name
+   }, function (err, quote) {
+     // get historical stock data for quotes and stock code
+     yahooFinance.historical({
+       symbol: name,
+       from: '2017-01-01',
+       to: new Date().toISOString().slice(0,10),
+       period: 'm' // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+     }, function (err, quotes) {
+       if (err) console.error(err);
+       else if (quotes.length === 0) console.error("stock does not exist");
+       else {
+         stockObj.name = quote.price.longName;
+         stockObj.values = quotes.map(obj => obj.close);
+         callback(stockObj);
+       }
+     });
+   });
+ }
+
+/**
+ * get all stocks in chartable format
+ * @param  {Function} callback callback function
+ */
+StockSchema.statics.getChartableStocks = function(callback) {
+  var Stock = this;
+  var arr = [];
+  Stock.find({}, function(err, stocks) {
+    if (err) console.error(err);
+    for (var i = 0; i < stocks.length; i++) {
+      Stock.getChartableStock(stocks[i].name, function(stockObj) {
+        arr.push(stockObj);
+        if (arr.length === stocks.length) callback(arr);
+      });
+    }
+  })
 }
 
 module.exports = mongoose.model('Stock', StockSchema);
